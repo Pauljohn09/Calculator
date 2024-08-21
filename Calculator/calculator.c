97bb6eadbc00f4c599841eb956e25ebe4aff6d93
Anora_TR_06\ARATHI K <arathik@anorasolutions.com>
2024-08-21 11:45:18 +0530

    my function

--- Calculator/calculator.c
+++ Calculator/calculator.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 
 //Add yor function declaration
-
+void primeNumber();
+void oddOrEven();
 
 int main()
 {
     //Call your function here 
     //Also give comments :Tell whos function this is .
-
-
-
-
+    void primeNumber();
+    void oddOrEven();
 
     return 0;
 }
