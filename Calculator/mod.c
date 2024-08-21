286cbcb107fea7c2a6442d7741bb00627b9fa972
DESKTOP-3B3AGVE\POOJA BIJOY <poojabijoy@anorasolutions.com>
2024-08-21 11:36:52 +0530

    adding functions to main

---
+++ Calculator/mod.c
@@ -0,0 +1,11 @@
+//#include <stdio.h>
+void mod()
+{
+    int x,y;
+    printf("Enter 2 numbers");
+    scanf("%d\n",&x);
+    scanf("%d",&y);
+    int res=x%y;
+    printf("Remainder=%d",res);
+    return;
+}
---
+++ Calculator/mul.c
@@ -0,0 +1,11 @@
+
+void mul()
+{
+    int x,y;
+    printf("Enter 2 number");
+    scanf("%d\n",&x);
+    scanf("%d",&y);
+    int res=x*y;
+    printf("Result=%d",res);
+    return;
+}
