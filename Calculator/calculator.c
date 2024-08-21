428c62d69a62dc6cec4f258ec26f100b84b7e2d0
Anora_TR_05\Paul John Thottungal <pauljohnthottungal@anorasolutions.com>
2024-08-21 13:15:53 +0530

    Deleting all lines

--- Calculator/calculator.c
+++ Calculator/calculator.c
@@ -1,24 +1,9 @@
 #include<stdio.h>
 
-void mul();
-void mod();
-void primeNumber();
-void oddOrEven();
-int fact(int);
-int permutations(int,int);
-int combinations(int,int);
-void RootsOfQuadraticEquation();
-void ReverseNumber();
-void square_of_number(int);
-void factorial(int);
 
 int main()
 {
 
-    square_of_number(5);
-    factorial(4);
-    void primeNumber();
-    void oddOrEven();
-
+   
     return 0;
 }
