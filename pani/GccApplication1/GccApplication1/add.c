a64a6b17af34dafea0625b2c776fc9acb36aa624
Anora_TR_01\YADHU KRISHNA T <yadhukrishnat@anorasolutions.com>
2024-08-21 14:52:25 +0530

    adding a microchip file

---
+++ pani/GccApplication1/GccApplication1/add.c
@@ -0,0 +1,11 @@
+/*
+ * add.c
+ *
+ * Created: 21-08-2024 14:13:10
+ *  Author: YADHU KRISHNA T
+ */ 
+int add(int a,int b)
+{
+	int c=a+b;
+	return c;
+}
---
+++ pani/GccApplication1/GccApplication1/main.c
@@ -0,0 +1,15 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 21-08-2024 14:08:41
+ * Author : YADHU KRISHNA T
+ */ 
+
+#include <avr/io.h>
+int add(int,int);
+int main()
+{
+	int a=add(5,3);	
+	return 0;
+}
+
