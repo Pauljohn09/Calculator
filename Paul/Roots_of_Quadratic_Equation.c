0b83395d01422abb7c446a27fb4bdc9c0fc97f66
Anora_TR_05\Paul John Thottungal <pauljohnthottungal@anorasolutions.com>
2024-08-21 09:55:14 +0530

    Updating the function "Roots of the quadratic Equation"

--- Paul/Roots_of_Quadratic_Equation.c
+++ Paul/Roots_of_Quadratic_Equation.c
@@ -1,24 +1,28 @@
-#include<stdio.h>
-#include<math.h>
-int main()
+
+void RootsOfQuadraticEquation()
 {
-int a =1, b=-16, c=1;
-float root1, root2;
-float d = b*b - 4*a*c;
-if(d>0)
-{
-    printf("The equation have real and unequal roots\n");
-    root1 = (float)(-b + sqrt(d))/(2*a);
-    root2 = (float)(-b - sqrt(d))/(2*a);
-    printf("The roots are %f and %f", root1, root2);
-}
-else if (d==0)
-{
-    printf("The roots are real and equal");
-    root1 = -b/(2*a);
-    printf("The root is %f", root1);
-}
-else
-    printf("The roots are complex conjugate");
-return 0;
+    int a,b,c;
+    float root1, root2;
+    printf("Enter the Coefficient of x^2 : ");
+    scanf("%d", &a);
+    printf("Enter the Coefficient of x : ");
+    scanf("%d", &b);
+    printf("Enter the constant term : ");
+    scanf("%d", &c);
+    float d = b*b - 4*a*c;
+    if(d>0)
+    {
+        printf("The equation have real and unequal roots\n");
+        root1 = (float)(-b + sqrt(d))/(2*a);
+        root2 = (float)(-b - sqrt(d))/(2*a);
+        printf("The roots are %f and %f", root1, root2);
+    }
+    else if (d==0)
+    {
+        printf("The roots are real and equal");
+        root1 = -b/(2*a);
+        printf("The root is %f", root1);
+    }
+    else
+        printf("The roots are complex conjugate");
 }
