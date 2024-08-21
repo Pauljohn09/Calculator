f7a44d78726138b3a32bececd71ade10260cdda9
Anora_TR_06\ARATHI K <arathik@anorasolutions.com>
2024-08-21 12:39:28 +0530

    primenumber

--- Calculator/calculator.c
+++ Calculator/calculator.c
@@ -2,7 +2,28 @@
 //Add yor function declaration
 void mul();
 void mod();
-void primeNumber();
+void primeNumber(){
+    int a,flag,i=0;
+     printf("Enter a positive integer: ");
+    scanf("%d", &a);
+    
+    if (a == 0 || a == 1) {
+        flag = 1;
+    }
+
+    for (i = 2; i <= sqrt(a); ++i) {
+        if (a % i == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0) {
+        printf("%d is a prime number.\n", a);
+    } else {
+        printf("%d is not a prime number.\n", a);
+    }
+}
 void oddOrEven();
 int fact(int);
 int permutations(int,int);
