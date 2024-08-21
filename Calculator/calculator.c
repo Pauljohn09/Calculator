f108e394428927d699eb7b89fa575a69c72dec0b
Anora_TR_05\Paul John Thottungal <pauljohnthottungal@anorasolutions.com>
2024-08-21 13:12:15 +0530

    ﻿Deleting all lines in calculator.c

--- Calculator/calculator.c
+++ Calculator/calculator.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 
 void mul();
 void mod();
@@ -13,27 +14,6 @@ void factorial(int);
 
 int main()
 {
-    //Call your function here 
-    //Also give comments :Tell whos function this is .
-
-   /*to multiply 2 numbers-Pooja*/
-   mul();
-   /*to find remainder of 2 numbers-Pooja*/
-   mod();
-   printf("enter something");
-
-    primeNumber();
-    oddOrEven();
-    printf("%d",permutations(5,4));
-    printf("%d",combinations(5,4));
-   //to find the prime number
-    primeNumber();
-    //to find the even or odd number
-    oddOrEven();
-
-    RootsOfQuadraticEquation();
-    ReverseNumber();
-
 
     square_of_number(5);
     factorial(4);
@@ -42,5 +22,3 @@ int main()
 
     return 0;
 }
-//Adding last line comment final time 
-//Adding this the last time
