7834eff59579dc4208eed259f5db552a971c9e76
DESKTOP-3B3AGVE\POOJA BIJOY <poojabijoy@anorasolutions.com>
2024-08-21 11:45:22 +0530

    function call edit

--- Calculator/calculator.c
+++ Calculator/calculator.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
 
 //Add yor function declaration
-
+void mul();
+void mod();
 
 int main()
 {
     //Call your function here 
     //Also give comments :Tell whos function this is .
 
+   /*to multiply 2 numbers-Pooja*/
+   mul();
+   /*to find remainder of 2 numbers-Pooja*/
+   mod();
 
 
 
